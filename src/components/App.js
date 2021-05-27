c3a48b251b2281fd95e4c44f3e8d1d534a0112bb
sreerama pavani <sreeram@sreeramas-MacBook-Pro.local>
2021-05-26 21:19:16 -0400

    implement unsplash api using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
+
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    onSubmit(term) {
-        console.log(term);
+    state = {results: []};
+
+    onSubmit = async (term) => {
+       const response = await unsplash.get('/search/photos', {
+            params: {
+                query: term
+            }
+        });
+        this.setState({results: response.data.results});
     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '20px'}}>
                 <SearchBar onSubmit={this.onSubmit}/>
+                Found {this.state.results.length} images
             </div>
         );
     }
